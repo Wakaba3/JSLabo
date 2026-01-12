64ae68d60e39d55113b730e7936d0ee67ddabcc3
Wakaba3 <219236104+Wakaba3@users.noreply.github.com>
2026-01-13 08:35:21 +0900

    Worker

--- docs/script.js
+++ docs/script.js
@@ -1,8 +1,6 @@
 const worker = new Worker("worker.js");
 
-addEventListener("pointerdown", event => {
-    worker.postMessage(null);
-});
+worker.postMessage(null);
 
 onmessage = event => {
     console.log(`Worker: ${event.data}`);
