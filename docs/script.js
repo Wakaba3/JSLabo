683ce9c50d1db8dc197c960e8712c0c6fc1d244d
Wakaba3 <219236104+Wakaba3@users.noreply.github.com>
2026-01-13 12:00:59 +0900

    Worker

--- docs/script.js
+++ docs/script.js
@@ -3,5 +3,7 @@ const worker = new Worker("worker.js");
 worker.postMessage(null);
 
 this.addEventListener("message", event => {
+    console.log("On main script");
+
     console.log(`Result: ${event.data}`);
 });
