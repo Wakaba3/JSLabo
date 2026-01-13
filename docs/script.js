6d5ac9ee9b5fbd3735df0e7165fdf554a9c9edf0
Wakaba3 <219236104+Wakaba3@users.noreply.github.com>
2026-01-13 11:33:33 +0900

    Worker

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,11 @@
 const worker = new Worker("worker.js");
 
+// Prevent right clicking
+addEventListener("contextmenu", event => event.preventDefault(), { passive: false });
+
+// Prevent touch scrolling
+addEventListener("touchmove", event => event.preventDefault(), { passive: false });
+
 addEventListener("message", event => {
     alert(`Worker: ${event.data}`);
 });
